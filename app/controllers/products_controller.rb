6e3f5c74506c567a581412ebf57063038c57e264
Saj <codersaj@gmail.com>
2017-09-02 23:04:12 +0000

    added styling

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,7 +7,7 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find params[:id]
-    @review = Review.find(@product.id)
+    @reviews = Review.where(product: @product)
     @newreview = Review.new
   end
 
